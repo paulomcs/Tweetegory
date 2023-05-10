bc9f2e63f76a1816028a0cc76d3b62038c23fcdd
Paulo Mateus Cunha Sampaio <52682759+paulomcs@users.noreply.github.com>
2023-05-10 17:32:53 -0300

    Add files via upload

---
+++ script.py.py
@@ -0,0 +1,91 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+import numpy as np
+import pandas as pd
+import nltk
+import spacy
+import string
+import re
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+pln = spacy.load("pt_core_news_sm")
+
+with open("tweets_Todos_processados.txt", 'r', encoding='unicode_escape') as base_original:
+    tweets_Todos_processados = base_original.readlines()
+
+
+# StopWords da Biblioteca Spacy
+stop_words_spacy = spacy.lang.pt.stop_words.STOP_WORDS
+
+# StopWords da Biblioteca 
+stop_words_nltk = stopwords.words('portuguese')
+
+# Lista com todos os caracteres que foram julgados indesejaveis
+caracteres_indesejaveis = ['—', 'de o', 'de a', 'a o', 'em o', 'em a', 'rt', '“', '”', '‘', '’', 'em', '):', 'R$']
+
+# simulando o tweet inserido pelo usuário
+tweet_usuario = "@joao_omotinha: Real Madrid x Machester City vai ser um belo jogo #champions"
+
+
+def processamento_texto(texto):
+  # Letra minúscula
+  texto = texto.lower()
+  # Remove menções (@)
+  #texto = re.sub(r'@\S+', '', texto)              
+  texto = re.sub(r"@[A-Za-z0-9$-_.&+]+", ' ', texto)
+  # Remove links
+  #texto = re.sub(r'http\S+', '', texto)
+  texto = re.sub(r"https?://[A-Za-z0-9./-]+", " ", texto)
+  # Remove hastags
+  texto = re.sub(r'#\S+', '', texto)
+  # Remove Emojis
+  emoji_pattern = re.compile("["u"\U0001F600-\U0001F64F"u"\U0001F300-\U0001F5FF"u"\U0001F680-\U0001F6FF"u"\U0001F1E0-\U0001F1FF"u"\U00002702-\U000027B0"u"\U000024C2-\U0001F251""]+", flags=re.UNICODE)
+  texto = emoji_pattern.sub(r'', texto)
+  # Numerais Ordinais
+  regex_numerais_ordinais = re.compile(r'\b\d+(?:º|ª)\b')
+  texto = regex_numerais_ordinais.sub('', texto)
+  # Números com vírgula
+  textp = re.sub(r'\b\d+,\d+\b', ' ', texto)
+  # Espaços em Branco
+  texto = re.sub(r" +", ' ', texto)
+  # RT
+  texto = texto.replace("rt ", " ")
+
+  #Lematização
+  documento = pln(texto)
+  lista = []
+  for token in documento:
+    lista.append(token.lemma_)
+  
+  # StopWords e Pontuações
+  lista = [palavra for palavra in lista if palavra not in stop_words_spacy and palavra not in stop_words_nltk and palavra not in string.punctuation and palavra not in caracteres_indesejaveis]
+
+  # Transforma em String removendo dígitos numéricos
+  lista = ' '.join([str(elemento) for elemento in lista if not elemento.isdigit()])
+
+  return lista
+
+# tweet do usuario processado
+tweet_usuario_processado = processamento_texto(tweet_usuario)
+
+# concatena base anterior com tweet do usuário
+tweets_Todos_processados.append(tweet_usuario_processado)
+
+# print(tweets_Todos_processados)
+
+vetor_palavras = TfidfVectorizer()
+
+
+palavras_vetorizadas = vetor_palavras.fit_transform(tweets_Todos_processados)
+
+
+kmeans_tweets = KMeans(n_clusters=5, n_init=15)
+kmeans_tweets.fit(palavras_vetorizadas)
+
+
+rotulos = kmeans_tweets.labels_
+
+rotulo_usuario = rotulos[-1]
+
+print(rotulo_usuario)
